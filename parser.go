2b004a9247bf44b76917523f00a974dac2f281aa
dsolerh <dsolerh.cinter95@gmail.com>
2023-01-24 11:59:04 +0200

    create a funtion to get the url content and parse it

--- parser.go
+++ parser.go
@@ -1,5 +1,27 @@
 package rssreader
 
+import (
+	"net/http"
+)
+
 func Parse(urls ...string) []RSSItem {
 	return nil
+}
+
+func fetchURL(url string, ch chan<- *RSSItem) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return
+	}
+	item, err := parseItem(resp.Body)
+	if err != nil {
+		return
+	}
+
+	ch <- item
 }
